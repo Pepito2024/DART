4454cdaae12754e7b657217dd24e5e0f9e07f082
John Bravo <021370b@utea.edu.pe>
2023-08-16 07:59:21 -0500

    primer commit

---
+++ condicionales.dart
@@ -0,0 +1,20 @@
+void main(){
+// int i = 0
+// while (i<5) {
+//   // instrucciones a repetir
+//   print("HOla");
+//   i++;
+// }
+// print ("fin  del while");
+int i = 0;
+do{
+  // instrucciones a peterir
+  print ("hola do while: $i");
+  i++;
+  // uso del break
+  if(i== 5) break;
+
+} while (i < 11);
+print ("fin de do while");
+
+}
---
+++ switch.dart
@@ -0,0 +1,33 @@
+void main(List <String> args){
+  //Switch
+  int dia = 2;
+  String hoy= "";
+
+  switch (dia) {
+    case 1:
+      hoy = "domingo";
+    break;
+    case 2:
+      hoy = "domingo";
+    break;
+    case 3:
+      hoy = "domingo";
+    break;
+    case 4:
+      hoy = "domingo";
+    break;
+    case 5:
+      hoy = "domingo";
+    break;
+    case 6:
+      hoy = "domingo";
+    break;
+    case 7:
+      hoy = "domingo";
+    break;
+
+    default:
+    hoy = "No";
+  }
+  print (hoy);
+}
