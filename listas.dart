5ab006f3478b7cbdc31478d863d1a5b5281d2c12
John Bravo <021370b@utea.edu.pe>
2023-08-18 08:38:14 -0500

    #tarea de clases

---
+++ listas.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  //lista (Array)
+  List items = [2, 5, 12, 5,"Brayan", "Hola", true];
+  List <int> numeros = [10, 15, 18, 12, 5];
+  List<String> nombres = []
+
+  print(numeros);
+
+}
--- string.dart
+++ string.dart
@@ -1,4 +1,4 @@
-void main(List<String> args) {
+//void main(List<String> args) {
 /*   //Colecciones
   String texto = 'Mi texto';
   String textoMultilinea = '''Mi texto
@@ -24,11 +24,46 @@ asi se configura
   print(texto);
   print(texto1); */
 
-  String nombre = "    Juan Gabriel Lopez    ";
+/*   String nombre = "    Juan Gabriel Lopez    ";
   print(nombre.isEmpty); //trato el string como un booleano
   print(nombre.length); //trato el string como un entero
   print(nombre.toUpperCase());
   print(nombre.toLowerCase());
   print(nombre.trim());
+  print(nombre.compareTo("    Juan Gabriel Lopez    "));
+  print(nombre.replaceAll("u", "e"));
+  print(nombre.substring(4,10));
+  print(nombre.contains("Juan"));
+  print(nombre.startsWith("A"));
+  print(nombre.endsWith("z"));
+
+String dni = "31037501";
+String name = "John";
+String lastname = "Bravo Villafuerte"; */
+
+/*
+1. Crear un codigo personalizado de usuario
+2. Primeras letras del nombre, las dos ultimas letras del apellido y 
+los 4 ultimos digitos del DNI.
+++++++++ Jhte7501 +++++++++
+*/
+
+void main() {
+  String dni = "31037501";
+  String name = "John";
+  String lastname = "Bravo Villafuerte";
+
+  String userCode = generateUserCode(name, lastname, dni);
+  print("Código personalizado: $userCode");
+}
+
+String generateUserCode(String firstName, String lastName, String dni) {
+  String firstInitial = firstName.substring(0, 1);
+  String lastTwoLetters = lastName.substring(lastName.length - 2);
+  String lastFourDigits = dni.substring(dni.length - 4);
+
+  String userCode = "$firstInitial$lastTwoLetters$lastFourDigits";
+  return userCode;
+
 
 }
